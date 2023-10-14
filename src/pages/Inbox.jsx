3ffd098a0c5b64ede75a286c04db406361806220
Faisal Karim <faisalchy64@gmail.com>
2023-10-14 15:21:47 +0600

    create - Inbox.jsx page

---
+++ src/pages/Inbox.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+export default function Inbox() {
+    return (
+        <>
+            <Navbar />
+            <main className="min-h-[calc(100vh-68px)]"></main>
+        </>
+    );
+}
