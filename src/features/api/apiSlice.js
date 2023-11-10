a6e0084997b6fcc807a5db0b5eddf2289b030ccc
Faisal Karim <faisalchy64@gmail.com>
2023-11-10 20:41:38 +0600

    add - customize baseQuery on apiSlice.js slice

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,19 +1,35 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { userSignout } from "../auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: "http://localhost:5000",
+    prepareHeaders: async (headers, { getState }) => {
+        const auth = getState().auth;
+
+        if (auth && auth.user) {
+            headers.set("Authorization", `Bearer ${auth.user.accessToken}`);
+        }
+
+        return headers;
+    },
+});
 
 const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000",
-        prepareHeaders: async (headers, { getState }) => {
-            const auth = getState().auth;
+    baseQuery: async (args, api, extraOptions) => {
+        const res = await baseQuery(args, api, extraOptions);
 
-            if (auth && auth.user) {
-                headers.set("Authorization", `Bearer ${auth.user.accessToken}`);
-            }
+        if (
+            res &&
+            res.error &&
+            (res.error.status === 401 || res.error.status === 403)
+        ) {
+            api.dispatch(userSignout());
+            localStorage.removeItem("auth");
+        }
 
-            return headers;
-        },
-    }),
+        return res;
+    },
     tagTypes: [],
     endpoints: () => ({}),
 });
